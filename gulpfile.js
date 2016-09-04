96288a2e1a5f9fc45068cdda85e328421b4b972c
ethan <ehaynes@g.harvard.edu>
2016-09-03 22:09:16 -0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var elixir = require('laravel-elixir');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+//  var paths = {
+//     'bootstrap': './vendor/twbs/bootstrap/dist/'
+// };
+
+elixir(function(mix) {
+    mix.sass('app.scss');
+    mix.styles('bootstrap.min.css',
+        'public/css/',
+        'vendor/twbs/bootstrap/dist/css'
+    );
+    mix.scripts('bootstrap.min.js',
+        'public/js/',
+        'vendor/twbs/bootstrap/dist/js'
+    );
+});
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="css/bootstrap.min.css" media="screen" title="no title">
+        <script src="js/bootstrap.min.js"></script>
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .inner {
+                width: 33em;
+            }
+
+            .title {
+                font-size: 96px;
+            }
+
+            .box {
+                background-color: lightgray;
+                height: 3em;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="row">
+
+        <div class="col-md-3"></div>
+            <div class="col-md-6 container">
+                <div class="content">
+                    <div class="title">Ethan Haynes</div>
+                    <span class="col-md-12 box"> </span>
+                    <div class="inner">ASsa ithsalkd nasjdn sjASsa ithsalkd nasjdn sjdna askjdb sakjd ak ynesdna askjdb sakjsalkd nasjdn sjASsa ithsa askjdb sakjd ak ynesdna askjdb sakjsalkd nasjdn sjASalkd nasd ak ynes ASsa ithsalkd nasjdn sjASsa ithsalkd nasjdn sjdna askjdb sakjd ak ynesdna askjdb sakjsalkd nasjdn sjASsa ithsa askjdb sakjd ak ynesdna askjdb sakjsalkd nasjdn sjASalkd nasd ak ynesASsa ithsalkd nasjdn sjASsa ithsalkd nasjdn sjdna askjdb sakjd ak ynesdna askjdb sakjsalkd nasjdn sjASsa ithsa askjdb sakjd ak ynesdna askjdb sakjsalkd nasjdn sjASalkd nasd ak ynesASsa ithsalkd nasjdn sjASsa ithsalkd nasjdn sjdna askjdb sakjd ak ynesdna askjdb sakjsalkd nasjdn sjASsa ithsa askjdb sakjd ak ynesdna askjdb sakjsalkd nasjdn sjASalkd nasd ak ynesASsa ithsalkd nasjdn sjASsa ithsalkd nasjdn sjdna askjdb sakjd ak ynesdna askjdb sakjsalkd nasjdn sjASsa ithsa askjdb sakjd ak ynesdna askjdb sakjsalkd nasjdn sjASalkd nasd ak ynesASsa ithsalkd nasjdn sjASsa ithsalkd nasjdn sjdna askjdb sakjd ak ynesdna askjdb sakjsalkd nasjdn sjASsa ithsa askjdb sakjd ak ynesdna askjdb sakjsalkd nasjdn sjASalkd nasd ak ynes</div>
+                </div>
+            </div>
+            <div class="col-md-3"></div>
+        </div>
+    </body>
+</html>
